2469605112e01f59ec620487b05cb8a28ea38427
Shane Jarman <sjarman91@gmail.com>
2016-09-09 17:13:19 -0400

    functional game

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -1,8 +1,4 @@
-$(document).ready(function (){
-	$('img').on('click', function() {
-		$(this).hide();
-	});
-});
+// JavaScript --------------------------------------------------
 
 // returns a random number bewteen 1 and 100
 function generateWinningNumber() {
@@ -65,32 +61,40 @@ Game.prototype.isLower = function() {
 // checks if the player has already guessed that number and for win/loss
 // also checks number of guesses
 Game.prototype.checkGuess = function() {
-	// guess does not count if you have already guessed it
-	// only pushes guess to array if it has not already been guessed
-	if (this.pastGuesses.indexOf(this.playersGuess) !== -1) {
-		return "You have already guessed that number.";
-	} else {
-		this.pastGuesses.push(this.playersGuess);
-	}
-
-	if (this.playersGuess === this.winningNumber) {
-		return "You Win!";
-	}
-
-	// "You Win" is tested first. if not activated, checks length of past guesses
-	// and tells you that you lose if you have 5 past guesses. Order matters.
-	if (this.pastGuesses.length === 5) {
-		return "You Lose.";
-	}
-
-	// to simplify code below
 	var diff = this.difference();
+	var lower = this.isLower();
+	var result = "";
 
 	// better way to write this? Cascading else/ifs to ensure only called once
-	if(diff < 10) {return "You're burning up!"}
-		else if(diff < 25) {return "You're lukewarm."} 
-		else if(diff < 50) {return "You're a bit chilly."}
-		else if(diff < 100) {return "You're ice cold!"}
+	if(diff < 10) {result += "You're burning up!"}
+		else if(diff < 25) {result += "You're lukewarm."} 
+		else if(diff < 50) {result += "You're a bit chilly."}
+		else if(diff < 100) {result += "You're ice cold!"}
+
+	if(lower) {result += " (guess higher)" } else {result += " (guess lower)"}
+
+	if (this.playersGuess === this.winningNumber) {
+		$('#player-input, #hint, #submit').prop('disabled', true);
+		$('#subtitle').text("Congrats! Hit reset to play again!");
+		return "You Win";
+		} 
+		else {
+			if (this.pastGuesses.indexOf(this.playersGuess) !== -1) {
+				return "You have already guessed that number.";
+			} 
+			else {
+				this.pastGuesses.push(this.playersGuess);
+				$('#guess-list li:nth-child('+ this.pastGuesses.length + ')').text(this.playersGuess);
+				if (this.pastGuesses.length === 5) {
+					$('#subtitle').text("Hit reset to play again!");
+					$('#player-input, #hint, #submit').prop('disabled', true);
+					return "You Lose!";
+				} else {
+					return result;
+				}
+			}
+
+		}
 }
 
 // accepts the players guess, assigns it to the object, and checks the guess
@@ -102,7 +106,6 @@ Game.prototype.playersGuessSubmission = function(guess) {
 	this.playersGuess = guess;
 	return this.checkGuess();
 } 
-
 
 // random numbers could be the actual winning number or they could both
 // be the same. Need to add a test to see if # is already in hint array
@@ -118,10 +121,60 @@ Game.prototype.provideHint = function() {
 	hintArray.push(rand1);
 	hintArray.push(rand2);
 
-	return shuffle(hintArray);
+	hintArray = shuffle(hintArray);
+
+	var hintMessage = "Try: " + hintArray[0] + " or " + hintArray[1] + " or " + hintArray[2];
+
+	return hintMessage;
 }
 
+// jQuery ----------------------------------------------------------------------
 
+function makeAGuess(gameVar) {
+	var guess = $('#player-input').val();
+	$('#player-input').val('');
+	var output = gameVar.playersGuessSubmission(parseInt(guess,10));
+	$('#title').text(output);
+};
 
+$(document).ready(function (){
+	
+	var game = new Game();
+	var hintGiven = false;
+	
+	$('img').on('click', function() {
+		$(this).hide();
+	});
 
+	$('#submit').click(function() {
+		makeAGuess(game);
+	});
 
+	$('#reset').click(function(){
+		game = new Game();
+
+		$('#player-input, #hint, #submit').prop('disabled', false);
+		$('#title').text('Guessing Game');
+		$('#subtitle').text('Guess a number between 1 and 100');
+		$('.guess').text("-");
+		$('#player-input').focus();
+		$('#userHint').remove();
+		hintGiven = false;
+	});
+
+	$('#hint').click(function() {
+		if(!hintGiven) {
+			$('#hint').after('<p id="userHint">'+ game.provideHint() + '</p>');
+		}
+		hintGiven = true;
+	});
+
+	// keyboard to click translation
+	$('#player-input').keypress(function(e) {
+		if(e.which == 13) {
+			$('#submit').click();	
+		}
+
+	});
+
+});
